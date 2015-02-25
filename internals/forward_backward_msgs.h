6586c2412a5c68fa4869e2cda435aa5995a4544f
Dillon Laird <dillonalaird@gmail.com>
2015-02-25 15:25:52 -0800

    internals now compiles, runs, and tests okay

--- internals/forward_backward_msgs.h
+++ internals/forward_backward_msgs.h
@@ -5,11 +5,8 @@
 #include <Eigen/Core>
 #include <limits>
 
+#include "util.h"
 #include "np_types.h"
-
-
-#define likely(x) __builtin_expect(!!(x), true)
-#define unlikely(x) __builtin_expect(!!(x), false)
 
 
 namespace fb {
@@ -17,47 +14,38 @@ namespace fb {
   using namespace Eigen;
   using namespace nptypes;
 
-  template<typename T> bool is_not_nan(const T& x) { return x == x; }
-  template<typename T> bool is_finite(const T& x) { return is_not_nan(x - x); }
-
-  template<typename Type>
-  void forward_msgs(int D, int T, Type* pi, Type* ltran, Type* lliks, Type* lalpha) {
+  template <typename Type>
+  void forward_msgs(int D, int T, Type* pi, Type* A, Type* lliks, Type* lalpha) {
     NPArray<Type>  e_pi(pi, 1, D);
-    NPMatrix<Type> e_ltran(ltran, D, D);
+    NPMatrix<Type> e_A(A, D, D);
     NPArray<Type>  e_lliks(lliks, T, D);
     NPArray<Type>  e_lalpha(lalpha, T, D);
 
     Type cmax;
     e_lalpha.row(0) = e_pi.log() + e_lliks.row(0);
 
-    for (int t = 0; t < T - 1; ++t) {
+    for (int t = 0; t < T - 1; t++) {
       cmax = e_lalpha.row(t).maxCoeff();
-      if (likely(is_finite(cmax))) {
-        e_lalpha.row(t + 1) = ((e_lalpha.row(t) - cmax).exp().matrix() * 
-            e_ltran).array().log() + e_lliks.row(t + 1) + cmax;
-      } else {
-        e_lalpha.block(t+1, 0, T - (t + 1), D).setConstant(
-            -numeric_limits<Type>::infinity());
-        return;
-      }
+      e_lalpha.row(t + 1) = ((e_lalpha.row(t) - cmax).exp().matrix() * 
+          e_A).array().log() + cmax + e_lliks.row(t + 1);
     }
   }
 
-  template<typename Type>
-  void backward_msgs(int D, int T, Type* ltran, Type* lliks, Type* lbeta) {
-    NPMatrix<Type> e_ltran(ltran, D, D);
-    NPArray<Type>  e_lliks(lliks, T, D);
-    NPArray<Type>  e_lbeta(lbeta, T, D);
+  template <typename Type>
+  void backward_msgs(int D, int T, Type* A, Type* lliks, Type* lbeta) {
+    NPMatrix<Type>  e_A(A, D, D);
+    NPMatrix<Type>  e_lliks(lliks, T, D);
+    NPMatrix<Type>  e_lbeta(lbeta, T, D);
 
     Type thesum_buf[D] __attribute__((aligned(16)));
     NPVector<Type> thesum(thesum_buf, D);
     Type cmax;
 
     e_lbeta.row(T - 1).setZero();
-    for (int t = 0; t < T - 2; ++t) {
+    for (int t = T - 2; t >= 0; t--) {
       thesum = (e_lliks.row(t + 1) + e_lbeta.row(t + 1)).transpose();
       cmax = thesum.maxCoeff();
-      e_lbeta.row(t) = (e_ltran * (thesum.array() - 
+      e_lbeta.row(t) = (e_A * (thesum.array() - 
             cmax).exp().matrix()).array().log() + cmax;
     }
   }
--- internals/forward_backward_msgs_interface.pyx
+++ internals/forward_backward_msgs_interface.pyx
@@ -6,17 +6,17 @@ cimport internals.forward_backward_msgs as fb_msgs
 
 # can't figure out how to use cython floating here
 def forward_msgs(np.ndarray[np.double_t, ndim=1, mode='c'] pi not None,
-                 np.ndarray[np.double_t, ndim=2, mode='c'] ltran not None,
+                 np.ndarray[np.double_t, ndim=2, mode='c'] A not None,
                  np.ndarray[np.double_t, ndim=2, mode='c'] lliks not None):
     cdef np.ndarray[np.double_t, ndim=2, mode='c'] lalpha = np.zeros_like(lliks)
-    fb_msgs.forward_msgs[np.double_t](ltran.shape[0], lliks.shape[0], &pi[0],
-            &ltran[0,0], &lliks[0,0], &lalpha[0,0])
+    fb_msgs.forward_msgs[np.double_t](A.shape[0], lliks.shape[0], &pi[0],
+            &A[0,0], &lliks[0,0], &lalpha[0,0])
     return lalpha
 
 
-def backward_msgs(np.ndarray[np.double_t, ndim=2, mode='c'] ltran not None,
+def backward_msgs(np.ndarray[np.double_t, ndim=2, mode='c'] A not None,
                   np.ndarray[np.double_t, ndim=2, mode='c'] lliks not None):
-    cdef np.ndarray[np.double_t, ndim=2, mode='c'] lbeta = np.zero_like(lliks)
-    fb_msgs.backward_msgs[np.double_t](ltran.shape[0], lliks.shape[0], 
-            &ltran[0,0], &lliks[0,0], &lbeta[0,0])
+    cdef np.ndarray[np.double_t, ndim=2, mode='c'] lbeta = np.zeros_like(lliks)
+    fb_msgs.backward_msgs[np.double_t](A.shape[0], lliks.shape[0], 
+            &A[0,0], &lliks[0,0], &lbeta[0,0])
     return lbeta
