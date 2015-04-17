76ca6b37a6a6db691c1642e0b53467d19b6a238f
Dillon Laird <dillonalaird@gmail.com>
2015-04-17 08:40:59 -0700

    overhauled gmm, fixed compilation errors

--- internals/gmm.h
+++ internals/gmm.h
@@ -3,30 +3,44 @@
 
 #include <vector>
 #include <iostream>
+#include <Eigen/Core>
+#include <Eigen/LU>
 #include "np_types.h"
+#include "eigen_types.h"
 
 
 namespace gmm {
   using namespace std;
   using namespace Eigen;
   using namespace nptypes;
+  using namespace eigentypes;
 
   static const constexpr double log2pi = 1.83787706640934533908193770912475883;
 
-  /* 
+  /*
    * Multivariate Gaussian Distribution.
+   *
+   * x
+   * e_mu
+   * sigma_inv
+   * lsigma_det
    */
-  template <typename Type>
-  Type _log_probability_m(NPVector<Type> x, NPVector<Type> e_mu, 
-                          NPMatrix<Type> sigma_inv, Type lsigma_det) {
-
-    NPArray<Type> diff = x - e_mu;
-    Type descriptive_stat = diff.transpose()*sigma_inv*diff;
-    return -0.5*(lsigma_det + descriptive_stat + e_mu.rows()*log2pi);
+  template <typename Type, typename Derived>
+  Type _log_probability_m(const ArrayBase<Derived>& x, const ArrayXt<Type> e_mu,
+      const MatrixXt<Type> sigma_inv, const Type lsigma_det) {
+    auto diff = (x - e_mu).matrix();
+    auto descriptive_stat = diff*sigma_inv*diff.transpose();
+    // NOTE: e_mu = [x_1, ..., x_n]
+    return -0.5*(lsigma_det + descriptive_stat + e_mu.cols()*log2pi);
   }
 
   /* 
    * Multivariate Gaussian Mixture Model.
+   *
+   * L
+   * x
+   * e_mus
+   * e_sigmas
    *
    * Notes:
    * Once this is tested pass in log sigma inverse and log sigma determinant so
@@ -36,23 +50,17 @@ namespace gmm {
    * eigen.tuxfamily.org/dox/TopicFunctionTakingEigenTypes.html
    */
   template <typename Type, typename Derived>
-  NPArray<Type> _log_probabilities_mm(int L, const ArrayBase<Derived>& x, 
-      NPArray<Type> e_cs, 
-      vector<NPArray<Type>, aligned_allocator<NPArray<Type> > > e_mus, 
-      vector<NPMatrix<Type>, aligned_allocator<NPMatrix<Type> > > e_sigma) {
+  ArrayXt<Type> _log_probabilities_mm(int L,
+      const ArrayBase<Derived>& x, const ArrayXt<Type>& e_c,
+      const vector<ArrayXt<Type>, aligned_allocator<ArrayXt<Type> > >& e_mus,
+      const vector<MatrixXt<Type>, aligned_allocator<MatrixXt<Type> > >& e_sigmas) {
 
-    Array<Type, Dynamic, Dynamic, RowMajor> lprobs(L, 1);
-    /*
+    auto lprobs = ArrayXt<Type>(1, L);
     for (int l = 0; l < L; ++l) {
-      // precalculate these
-      NPMatrix<Type> lsigma_inv = e_sigma[l].inverse().log();
-      Type lsigma_det = e_sigma[l].determinant().log();
-
-      lprobs[l] = e_cs[l].log() + _log_probability_m(x, e_mus[l], lsigma_inv,
-          lsigma_det);
+      auto sigma_inv = e_sigmas[l].inverse();
+      auto lsigma_det = log(e_sigmas[l].determinant());
+      lprobs(1, l) = log(e_c.coeff(l)) + _log_probability_m<Type>(x, e_mus[l], sigma_inv, lsigma_det);
     }
-    */
-
     return lprobs;
   }
 
@@ -78,40 +86,35 @@ namespace gmm {
    * expected_counts - An S x L NPMatrix containing the expected counts.
    *
    * Notes:
-   * expected_x2 = \sum_{t=1}^T \gamma_{s,l}(t)*o_t*o_t^T
-   * expectex_x = \sum_{t=1}^T \gamma_{s,l}*o_t
+   * expected_x2     = \sum_{t=1}^T \gamma_{s,l}(t)*o_t*o_t^T
+   * expectex_x      = \sum_{t=1}^T \gamma_{s,l}*o_t
    * expected_counts = \sum_{t=1}^T \gamma_{s,l}
-   *
-   * This might be more of an internal function since the parameters are all
-   * Eigen containers (you can't call this from Python). This is so we don't
-   * have to refill the Eigen containers again from update_parameters.
    */
   template <typename Type>
-  void _weighted_sufficient_statistics(int S, int T, int D, int L, 
-      vector<NPArray<Type>, aligned_allocator<NPArray<Type> > > e_cs,
-      vector<vector<NPArray<Type>, aligned_allocator<NPArray<Type> > > > e_mus,
-      vector<vector<NPMatrix<Type>, aligned_allocator<NPMatrix<Type> > > > e_sigmas,
-      NPArray<Type> e_obs, NPArray<Type> e_lweights,
+  void _weighted_sufficient_statistics(int S, int T, int D, int L,
+      const vector<ArrayXt<Type>, aligned_allocator<ArrayXt<Type> > >& e_cs,
+      const vector<vector<ArrayXt<Type>, aligned_allocator<ArrayXt<Type> > > >& e_mus,
+      const vector<vector<MatrixXt<Type>, aligned_allocator<MatrixXt<Type> > > >& e_sigmas,
+      const NPArray<Type>& e_obs, const NPArray<Type>& e_lweights,
 
-      vector<vector<NPMatrix<Type>, aligned_allocator<NPMatrix<Type> > > > expected_x2,
-      vector<vector<NPArray<Type>, aligned_allocator<NPArray<Type> > > > expected_x,
-      NPMatrix<Type> expected_count) {
+      vector<vector<MatrixXt<Type>, aligned_allocator<MatrixXt<Type> > > >& expected_x2,
+      vector<vector<ArrayXt<Type>, aligned_allocator<ArrayXt<Type> > > >& expected_x,
+      MatrixXt<Type>& expected_count) {
 
-    Type lprobs_buff[L] __attribute__((aligned(16)));
-    NPArray<Type> lprobs(lprobs_buff, L, 1);
-
-    Type x2_buff[D*D] __attribute__((aligned(16)));
-    NPMatrix<Type> x2(x2_buff, D, D);
+    auto lprobs = ArrayXt<Type>(1, L);
+    auto component_weight = ArrayXt<Type>(1, L);
+    auto x2 = MatrixXt<Type>(D, D);
 
     for (int t = 0; t < T; ++t) {
-      x2 = e_obs.row(t)*e_obs.row(t);
+      x2 = e_obs.row(t)*e_obs.row(t).transpose();
       for (int s = 0; s < S; ++s) {
-        lprobs = _log_probabilities_mm<Type>(L, e_obs.row(t), e_cs[s],
-            e_mus[s], e_sigmas[s]);
-        //expected_count.row(s) += e_lweights.coeff(t, s) + lprobs;
+        lprobs = _log_probabilities_mm<Type>(L, e_obs.row(t), e_cs[s], e_mus[s], e_sigmas[s]);
+        component_weight = (exp(e_lweights.coeff(t, s))*lprobs.exp())/lprobs.exp().sum();
+
+        expected_count.row(s) += component_weight.matrix();
         for (int l = 0; l < L; ++l) {
-          //expected_x2[s][l] += (e_lweights.coeff(t, s) + lprobs[l]).exp()*x2;
-          //expected_x[s][l] += (e_lweights.coeff(t, s) + lprobs[l]).exp()*e_obs.row(t);
+          expected_x2[s][l] += component_weight.coeff(l)*x2;
+          expected_x[s][l] += component_weight.coeff(l)*e_obs.row(t);
         }
       }
     }
@@ -142,43 +145,38 @@ namespace gmm {
     int i2;
     int i3;
 
-    vector<NPArray<Type>, aligned_allocator<NPArray<Type> > > e_cs;
-    for (i1 = 0, i2 = 0; i1 < S, i2 < S*L; ++i1, i2 += L)
-      e_cs[i1] = NPArray<Type>(&cs[i2], L, 1);
-
-    vector<vector<NPArray<Type>, aligned_allocator<NPArray<Type> > > > e_mus;
-    for (i1 = 0, i2 = 0, i3 = 0; i1 < S, i2 < L, i3 < S*L*D; ++i1, ++i2, i3 += D)
-      e_mus[i1][i2] = NPArray<Type>(&cs[i3], D, 1);
-
-    vector<vector<NPMatrix<Type>, aligned_allocator<NPMatrix<Type> > > > e_sigmas;
-    for (i1 = 0, i2 = 0, i3 = 0; i1 < S, i2 < L, i3 < S*L*D*D; ++i1, ++i2, i3 += D*D)
-      e_sigmas[i1][i2] = NPMatrix<Type>(&sigmas[i3], D, D);
-
     NPArray<Type> e_obs(obs, T, D);
     NPArray<Type> e_lweights(lweights, T, S);
 
-    // containers for weighted sufficient statistics
-    vector<vector<NPMatrix<Type>, aligned_allocator<NPMatrix<Type> > > > expected_x2;
-    for (i1 = 0, i2 = 0; i1 < S, i2 < L; ++i1, ++i2) {
-      Type expected_x2_buff[D*D] __attribute__((aligned(16)));
-      expected_x2[i1][i2] = NPMatrix<Type>(expected_x2_buff, D, D);
-    }
+    vector<ArrayXt<Type>, aligned_allocator<ArrayXt<Type> > > e_cs;
+    for (i1 = 0; i1 < S; ++i1)
+      for (i2 = 0; i2 < S*L; i2 += L)
+        e_cs[i1] = NPArray<Type>(&cs[i2], L, 1);
 
-    vector<vector<NPArray<Type>, aligned_allocator<NPArray<Type> > > > expected_x;
-    for (i1 = 0, i2 = 0; i1 < S, i2 < L; ++i1, ++i2) {
-      Type expected_x_buff[D] __attribute__((aligned(16)));
-      expected_x[i1][i2] = NPArray<Type>(expected_x_buff, D, 1);
-    }
+    vector<vector<ArrayXt<Type>, aligned_allocator<ArrayXt<Type> > > > e_mus;
+    for (i1 = 0; i1 < S; ++i1)
+      for (i2 = 0; i2 < L; ++i2)
+        e_mus[i1][i2] = NPArray<Type>(&mus[(i1+1)*(i2+1)*D], D, 1);
 
-    Type expected_counts_buff[S*L] __attribute__((aligned(16)));
-    NPMatrix<Type> expected_counts(expected_counts_buff, S, L);
+    vector<vector<MatrixXt<Type>, aligned_allocator<MatrixXt<Type> > > > e_sigmas;
+    for (i1 = 0; i1 < S; ++i1)
+      for (i2 = 0; i2 < L; ++i2)
+        e_sigmas[i1][i2] = NPMatrix<Type>(&sigmas[(i1+1)*(i2+1)*D*D], D, D);
 
-    _weighted_sufficient_statistics(S, T, D, L, e_cs, e_mus, e_sigmas, e_obs, 
+    vector<vector<MatrixXt<Type>, aligned_allocator<MatrixXt<Type> > > > expected_x2;
+    for (i1 = 0; i1 < S; ++i1)
+      for (i2 = 0; i2 < L; ++i2)
+        expected_x2[i1][i2] = MatrixXt<Type>(D, D);
+
+    vector<vector<ArrayXt<Type>, aligned_allocator<ArrayXt<Type> > > > expected_x;
+    for (i1 = 0; i1 < S; ++i1)
+      for (i2 = 0; i2 < L; ++i2)
+        expected_x[i1][i2] = ArrayXt<Type>(1, D);
+
+    auto expected_counts = MatrixXt<Type>(S, L);
+
+    _weighted_sufficient_statistics(S, T, D, L, e_cs, e_mus, e_sigmas, e_obs,
         e_lweights, expected_x2, expected_x, expected_counts);
-
-    for (int s = 0; s < S; ++s)
-      for (int l = 0; l < L; ++l) {
-      }
   }
 }
 
