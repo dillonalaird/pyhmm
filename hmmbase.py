498a7f65298605de4f319802828b65e59d912e86
dillon <dillonalaird@gmail.com>
2015-04-28 12:15:31 -0700

    starting to build baum welch

--- hmmbase.py
+++ hmmbase.py
@@ -3,6 +3,7 @@ from __future__ import division
 import abc
 import numpy as np
 import forward_backward as fb
+import gmm
 
 
 class HMMBase(object):
@@ -19,7 +20,7 @@ class HMMBase(object):
 
         pass
 
-    def __init__(self, obs, K, pi, A):
+    def __init__(self, obs, S, pi, A, L):
         """
         Initialize the HMM object.
 
@@ -28,7 +29,7 @@ class HMMBase(object):
         obs : numpy.array
             A T x D numpy array of T observations in D dimensions.
 
-        K : int
+        S : int
             The number of hidden states to use.
 
         pi : numpy.array
@@ -36,38 +37,66 @@ class HMMBase(object):
 
         A : numpy.array
             A K x K matrix representing the transition matrix.
+
+        L : int
+            The number of mixtures for the Gaussian Mixture Model.
         """
 
         self.obs = obs
         self.pi = pi
         self.A = A
 
+        # initialize GMM parameters
+        self.cs
+        self.mus
+        self.sigmas
 
-    def EM(self):
-        # Expectation Step
-        self.log_likelihoods()
-        self.forward_msgs()
-        self.backward_msgs()
-        self.expected_statistics()
+    def Baum_Welch(self):
+        for itr in iters:
+            # Expectation Step
+            self._forward_msgs()
+            self._backward_msgs()
+            self._log_weights()
+            lexpected_states, expected_transcounts = self._expected_statistics()
 
-        # Maximization Step
+            # Maximization Step
+            expected_states = np.exp(lexpected_states)
+            expected_states = epxected_states/np.sum(expected_states, axis=1)[:,np.newaxis]
 
-    def forward_msgs(self):
+            self.pi = expected_states[0]
+            self.A = expected_transcounts/ \
+                    np.sum(expected_states[:len(expected_states)-1], axis=0)[:,np.newaxis]
+
+            gmm.update_parameters(self.cs, self.mus, self.sigmas, self.obs, self.lweights)
+
+
+    def _forward_msgs(self):
         """
         """
 
         self.lalpha = fb.forward_msgs(self.pi, self.A, self.lliks)
 
-    def backward_msgs(self):
+    def _backward_msgs(self):
         """
         """
 
         self.lbeta = fb.backward_msgs(self.A, self.lliks)
 
-    def expected_statistics(self):
+    def _log_likelihood(self):
         """
         """
 
-        self.expected_states, self.expected_transcounts = \
-            fb.expected_statistics(self.pi, self.A, self.lliks, self.lalpha, 
-                                   self.lbeta)
+        pass
+
+    def _log_weights(self):
+        """
+        """
+
+        self.lweights = fb.log_weights(self.lalpha, self.lbeta)
+
+    def _expected_statistics(self):
+        """
+        """
+
+        return fb.expected_statistics(self.pi, self.A, self.lliks,
+                self.lalpha, self.lbeta)
