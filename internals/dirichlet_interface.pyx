3a26087b8dcc5f77a32ebe5c98d70064db1038d2
Dillon Laird <dillonalaird@gmail.com>
2015-07-18 16:39:50 -0700

    corrected incorrect name

--- internals/dirichlet_interface.pyx
+++ internals/dirichlet_interface.pyx
@@ -4,7 +4,7 @@ cimport numpy as np
 cimport internals.dirichlet as dir
 
 
-def exepected_sufficient_statistics(np.ndarray[np.double_t, ndim=2, mode='c'] alphas not None):
+def expected_sufficient_statistics(np.ndarray[np.double_t, ndim=2, mode='c'] alphas not None):
     cdef int S = alphas.shape[0]
     cdef np.ndarray[np.double_t, ndim=2, mode='c'] ess = np.zeros_like(alphas)
     dir.expected_sufficient_statistics[np.double_t](S, &alphas[0,0], &ess[0,0])
