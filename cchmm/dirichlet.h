e5b451180cfa03bb84ec7b3d4724388f559d6759
Dillon Laird <dillonalaird@gmail.com>
2015-08-23 12:35:54 -0700

    fixed parameters for meanfield update

--- cchmm/dirichlet.h
+++ cchmm/dirichlet.h
@@ -46,11 +46,11 @@ namespace dir {
     }
 
     template <typename Type>
-    void meanfield_sgd_update(Type lrate, Type bfactor,
-                              const NPMatrix<Type>& A_nat_0,
-                              NPMatrix<Type>& A_nat_N,
-                              const MatrixXt<Type>& ess) {
-        A_nat_N = (1 - lrate)*A_nat_N + lrate*(A_nat_0 + bfactor*ess);
+    MatrixXt<Type> meanfield_sgd_update(Type lrate, Type bfactor,
+                                        const NPMatrix<Type>& A_nat_0,
+                                        const NPMatrix<Type>& A_nat_N,
+                                        const MatrixXt<Type>& ess) {
+        return (1 - lrate)*A_nat_N + lrate*(A_nat_0 + bfactor*ess);
     }
 }
 
