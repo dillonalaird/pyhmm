0d8ce0031fa800f3c20a6fe98596a2f035cb8681
Dillon Laird <dillonalaird@gmail.com>
2015-09-04 08:16:04 -0700

    added method for converting emissions back to 2d matrix

--- cchmm/hmmsvi_interface.pyx
+++ cchmm/hmmsvi_interface.pyx
@@ -4,13 +4,13 @@ cimport numpy as np
 cimport cchmm.hmmsvi as hmmsvi
 
 
-def convert_emits(S, D, emits):
+def flatten_emits(S, D, emits):
     cdef np.ndarray[np.double_t, ndim=1, mode='c'] _emits = np.zeros(S*(D*D + 3*D))
 
     cdef int s = 0
     cdef int offset1 = D*D + 3*D
     cdef int offset2 = D + 3
-    for s in range(S):
+    for s in xrange(S):
         _emits[(s*offset1):(s*offset1 + D*D)] = \
                 emits[(s*offset2):(s*offset2 + D)].flatten().copy(order='C')
         _emits[(s*offset1 + D*D):(s*offset1 + D*D + D)] = \
@@ -19,6 +19,25 @@ def convert_emits(S, D, emits):
         _emits[(s*offset1 + D*D + 2*D)] = emits[(s*offset2 + D + 2),0]
 
     return _emits
+
+
+def explode_emits(S, D, emits):
+    cdef np.ndarray[np.double_t, ndim=2] _emits
+    cdef np.ndarray[np.double_t, ndim=2] _emits_i
+
+    cdef int s = 0
+    cdef int offset1 = D*D + 3*D
+    cdef int offset2 = D + 3
+    for s in xrange(S):
+        _emits_i        = np.zeros((D + 3, D))
+
+        _emits_i[:D,:D] = emits[(s*offset1):(s*offset1 + D*D)].reshape((D, D))
+        _emits_i[D,:]   = emits[(s*offset1 + D*D):(s*offset1 + D*D + D)]
+        _emits_i[D+1,0] = emits[(s*offset1 + D*D + D)]
+        _emits_i[D+2,0] = emits[(s*offset1 + D*D + 2*D)]
+        _emits = _emits_i if s == 0 else np.vstack((_emits, _emits_i))
+    return _emits
+
 
 def infer(np.ndarray[np.double_t, ndim=2, mode='c'] obs,
           np.ndarray[np.double_t, ndim=2, mode='c'] A_0,
@@ -31,9 +50,11 @@ def infer(np.ndarray[np.double_t, ndim=2, mode='c'] obs,
 
     cdef np.ndarray[np.double_t, ndim=2, mode='c'] A_N = A_0.copy(order='C')
     cdef np.ndarray[np.double_t, ndim=1, mode='c'] _emits_0 = \
-            convert_emits(S, D, emits_0)
+            flatten_emits(S, D, emits_0)
     cdef np.ndarray[np.double_t, ndim=1, mode='c'] _emits_N = \
-            convert_emits(S, D, emits_0)
+            flatten_emits(S, D, emits_0)
 
     hmmsvi.infer[np.double_t](D, S, T, &obs[0,0], &A_0[0,0], &A_N[0,0],
                               &_emits_0[0], &_emits_N[0], tau, kappa, L, n, itr)
+    emits_N = explode_emits(S, D, _emits_N)
+    return A_N, emits_N
